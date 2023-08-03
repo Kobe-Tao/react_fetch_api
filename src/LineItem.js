11dfabc8b6cc0a81936ab66243b935602ac34c94
lutao <lutaoapply2023@gmail.com>
2023-08-02 22:06:00 -0400

    react_fetchapi

---
+++ src/LineItem.js
@@ -0,0 +1,23 @@
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label style={item.checked ? { textDecoration: "line-through" } : null}>
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        arial-label={`Delete ${item.item}`}
+      />
+    </li>
+  );
+};
+
+export default LineItem;
